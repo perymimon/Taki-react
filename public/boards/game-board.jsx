ead66a7a98c9896d7853e88c3baa793dd569ebd1
pery mimon <pery.mimon@gmail.com>
2019-01-20 14:05:49 +0200

    savepoint: add glow to board in taki mode

--- public/boards/game-board.jsx
+++ public/boards/game-board.jsx
@@ -1,19 +1,29 @@
 import './game-board.scss';
 import React, {Component} from "react";
 import Card from '../directives/card'
+import get from 'lodash/get';
+
+
+const {GAME_MODE} = require('../../common/game-consts');
 
 import {connect} from 'unistore/src/integrations/react';
 // import  {connect} from 'unistore/src/integrations/react';
 import {store, actions} from '../store/store';
+import {classnames} from '../utils/utils';
 
 
-export default connect('stack, deck, stackLay')(
-    function GameBoard({stack, deck}) {
+export default connect('stack, deck, mode')(
+    function GameBoard({stack, deck, mode}) {
         var topStackCards = [...stack.topCards];
+        var topCard = stack.topCards[0];
+        const classState = classnames({
+            'taki-mode': (mode === GAME_MODE.TAKI),
+        });
         return (
-            <board-game>
+            <board-game class={`${classState}  ${get(topCard, 'card.color')}`}>
+                <tk-text>TAKI</tk-text>
                 <tk-card class="stack self-center">
-                    {topStackCards.reverse().map(({card,lay}) => {
+                    {topStackCards.reverse().map(({card, lay}) => {
                         return <Card card={card} lay={lay} key={card.id}/>
                     })}
                 </tk-card>
